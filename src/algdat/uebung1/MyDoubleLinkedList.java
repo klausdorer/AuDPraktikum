5ec632ed0a9fe8a5c2876671a9abfeb44beb6baa
Klaus Dorer <klaus.dorer@hs-offenburg.de>
2019-03-07 15:55:41 +0100

    New exercise for AI with Java knowledge

---
+++ src/algdat/uebung1/MyDoubleLinkedList.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2007 Klaus Dorer
+ * Hochschule Offenburg
+ */
+package algdat.uebung1;
+
+/**
+ * Class representing a list of int values based on double linking. The first
+ * element is a dummy element to simplify inserting/deleting
+ */
+public class MyDoubleLinkedList
+{
+	public MyDoubleLinkedList()
+	{
+		// TODO: please put your code here
+	}
+
+	/**
+	 * Returns the number of elements in this list
+	 * @return the number of elements in this list
+	 */
+	public int size()
+	{
+		// TODO: please put your code here
+		return 0;
+	}
+
+	/**
+	 * Returns the value at the position specified
+	 * @param position the 0 based position of the value to return
+	 * @return the value at the passed position, -1 if unsuccessful. This means
+	 *         that this implementation is not working properly if -1 is stored
+	 *         in the list
+	 */
+	public int get(int position)
+	{
+		// TODO: please put your code here
+		return 0;
+	}
+
+	/**
+	 * Adds a new element into the list at the position specified
+	 * @param position the 0 based position at which to add the passed value
+	 * @param value the value to add
+	 * @return 0 if adding was successful, -1 if not
+	 */
+	public int add(int position, int newElement)
+	{
+		// TODO: please put your code here
+		return 0;
+	}
+
+	/**
+	 * Removes an element at the position specified from the list
+	 * @param position the 0 based position of the value to remove
+	 * @return value of the removed entry if removing was successful, -1 if not
+	 */
+	public int remove(int position)
+	{
+		// TODO: please put your code here
+		return 0;
+	}
+
+	/**
+	 * Searches for the first occurrence of the passed value in the list
+	 * @param value the value to search
+	 * @return the position of the value in the list, -1 if not found
+	 */
+	public int indexOf(int value)
+	{
+		// TODO: please put your code here
+		return 0;
+	}
+
+	/**
+	 * Prints the numbers in the list to console as comma separated String
+	 */
+	public void print()
+	{
+		System.out.print("List: ");
+		// TODO: please put your code here
+	}
+}
---
+++ src/algdat/uebung1/MyDoubleLinkedListTest.java
@@ -0,0 +1,22 @@
+package algdat.uebung1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Class for unit testing MyDoubleLinkedList
+ */
+public class MyDoubleLinkedListTest
+{
+	@Before
+	public void setUp() throws Exception
+	{
+		// TODO create data you need for all Tests here
+	}
+
+	@Test
+	public void testAddInvalidIndex()
+	{
+		// assertEquals(-1, testee.add(-1, 10));
+	}
+}
